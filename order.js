5359321afb4781094595f170dea24957b6b0d243
Suvetha-14 <suvetha7300@gmail.com>
2025-04-03 22:13:49 +0530

    updated order.js

---
+++ order.js
@@ -0,0 +1,11 @@
+const urlParams = new URLSearchParams(window.location.search);
+document.getElementById('product').value = urlParams.get('product');
+document.getElementById('price').value = urlParams.get('price');
+
+document.getElementById('orderForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    document.getElementById('orderNotification').style.display = 'block';
+    setTimeout(() => {
+        window.location.href = 'index.html';
+    }, 2000);
+});
